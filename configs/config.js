c7bd152bbeca8e5fa3bf40f85989a1cd032fb441
vikas gupta <vikasgupta@zapbuild.com>
2021-04-02 13:04:46 +0530

    add custom function

--- configs/config.js
+++ configs/config.js
@@ -2,4 +2,19 @@ module.exports = {
   PORT: process.env.PORT,
   HOST_URL: process.env.HOST_URL,
   swaggerHost: process.env.SWAGGER_HOST,
+  i18n: {
+    locales: ["en"],
+    updateFiles: false,
+    directory: __dirname + "/locales",
+  },
+  mailerSettings: {
+    host: process.env.EMAIL_SERVICE,
+    port: process.env.EMAIL_PORT,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  },
+  fromEmail: process.env.FROM_EMAIL,
 };
